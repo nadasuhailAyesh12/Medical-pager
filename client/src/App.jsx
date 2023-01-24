f77cf53ce7225c9aeafc27af62ea45f49fdd5f12
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2023-01-24 19:32:05 +0200

    connect the new user to stream

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,35 @@
-import React from "react";
-import { StreamChat } from "stream-chat";
-import { Chat } from "stream-chat-react";
-import Cookies from "universal-cookie";
+import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
 
-import "./App.css";
-import { ChannelListContainer, ChannelContainer } from "./Components";
+import './App.css'
+import { ChannelListContainer, ChannelContainer, Auth } from './Components'
 
-const key = "1230535";
+const cookies = new Cookies()
 
-const client = StreamChat.getInstance(key);
+const key = 'sh2jd2ujh634'
+const authToken = cookies.get('token')
+
+const client = StreamChat.getInstance(key)
+
+if (authToken) {
+  client.connectUser(
+    {
+      id: cookies.get('userId'),
+      name: cookies.get('username'),
+      fullName: cookies.get('fullName'),
+      image: cookies.get('avatarURL'),
+      hashedPassword: cookies.get('hashedPassword'),
+      phoneNumber: cookies.get('phoneNumber'),
+    },
+    authToken,
+  )
+}
 
 function App() {
+  if (!authToken) return <Auth />
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
@@ -18,7 +37,7 @@ function App() {
         <ChannelContainer />
       </Chat>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
