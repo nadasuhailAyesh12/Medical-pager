9ff22c8090f6d2418c2e4c3e763b178c29a6c471
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2023-01-26 04:09:21 +0200

    make sidebar functionality

--- client/src/Components/TeamChannelPreview.jsx
+++ client/src/Components/TeamChannelPreview.jsx
@@ -1,45 +1,61 @@
-import React from "react";
-import { Avatar, useChatContext } from "stream-chat-react";
-function TeamChannelPreview({ channel, type }) {
-  const { channel: activeChannel, client } = useChatContext();
+import React from 'react'
+import { Avatar, useChatContext } from 'stream-chat-react'
+function TeamChannelPreview({
+  setActiveChannel,
+  channel,
+  type,
+  setToggleContainer,
+  setIsCreating,
+  setIsEditing,
+}) {
+  const { channel: activeChannel, client } = useChatContext()
 
   function ChannelPreview() {
     return (
       <p className="channel-preview__item">
         {channel?.data?.name || channel?.data?.id}
       </p>
-    );
+    )
   }
 
   function DirectPreview() {
-    const members = Object.values(channel.state.members)
-      .filter(({ user }) => user.id !== client.userID);
+    const members = Object.values(channel.state.members).filter(
+      ({ user }) => user.id !== client.userID,
+    )
+
     return (
       <div className="channel-preview__item single">
         <Avatar
           image={members[0]?.user?.image}
-          name={members[0]?.user?.fullName}
+          name={members[0]?.user?.fullName||members[0]?.user?.id}
           size={24}
         />
         <p> {members[0]?.user?.fullName}</p>
       </div>
-    );
+    )
   }
 
   return (
     <div
       className={
         channel.id === activeChannel.id
-          ? "channel-preview__ wrapper__selected"
-          : "channel-preview__ wrapper"
+          ? 'channel-preview__wrapper__selected'
+          : 'channel-preview__wrapper'
       }
+
       onClick={() => {
-        console.log(channel);
+        setIsCreating(false)
+        setIsEditing(false)
+        setActiveChannel(channel)
+
+        if (setToggleContainer) setToggleContainer((prevToggle) => !prevToggle)
+
       }}
     >
-      {(type = "team" ? <ChannelPreview /> : <DirectPreview />)}
+      {type === 'team' ? <ChannelPreview /> : <DirectPreview />}
+
     </div>
-  );
+  )
 }
 
-export default TeamChannelPreview;
+export default TeamChannelPreview
